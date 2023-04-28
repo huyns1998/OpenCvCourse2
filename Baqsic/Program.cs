ee9a468411f65c040a462069b5807697dfe26af3
Huyns <syhuynguyen1@gmail.com>
2023-04-28 14:29:27 +0700

    dd

--- Baqsic/Program.cs
+++ Baqsic/Program.cs
@@ -47,7 +47,7 @@ namespace Baqsic
             //Cv2.BilateralFilter(image, bilDst1, 9, 75, 75);
             //Cv2.BilateralFilter(image, bilDst2, 9, 75, 2);
             //Cv2.MedianBlur(image, medianBlur, 5);
-            //Cv2.ImShow("bilDst1", bilDst1);
+            Cv2.ImShow("bilDst1", bilDst1);
             Cv2.ImShow("bilDst2", bilDst2);
             Cv2.ImShow("medianBlur", medianBlur1);
 
